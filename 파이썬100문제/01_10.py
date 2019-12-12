6b9cc1faac5596bb69a02d623256889945180422
정해성 <briskly0415@gmail.com>
2019-12-12 09:50:43 +0900

    추가

--- 파이썬100문제/01_10.py
+++ 파이썬100문제/01_10.py
@@ -4,8 +4,10 @@ nums = [100, 200, 300, 400, 500]
 
 print(nums)
 
-nums.remove(400)
-nums.remove(500)
+# nums.remove(400)
+# nums.remove(500)
+
+nums = nums[:3]
 
 print(nums)
 
@@ -39,7 +41,12 @@ print(a+b)
 # 다음은 파이썬 문법 중에서 False로 취급하는 것들 입니다.
 # 앗, False로 취급하지 않는 것이 하나 있네요! True를 찾아주세요.
 
-print(None)
+print(type(None))
+
+if None:
+    print('False')
+else:
+    print('True')
 
 # 문제7 : 변수명
 # 다음 중 변수명으로 사용할 수 없는 것 2개를 고르시오.
